640fe3ae163a4076e9b0b29b4ca31670eac92843
mohitsharma990 <iitgmohitsharma@gmail.com>
2024-08-24 12:04:55 +0530

    q4 solution revised

--- DOMjudge_PracticeQs_ProgTest1/q4.cpp
+++ DOMjudge_PracticeQs_ProgTest1/q4.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 using namespace std;
 
+const int mod = 1e9 + 7;
+
 vector<vector<long long>> matMul(vector<vector<long long>> A, vector<vector<long long>> B) {
   vector<vector<long long>> result;
   for(int i=0; i<100; i++) {
@@ -13,7 +15,7 @@ vector<vector<long long>> matMul(vector<vector<long long>> A, vector<vector<long
     for(int j=0; j<100; j++) {
       long long temp = 0;
       for(int k=0; k<100; k++) {
-        temp += (A[i][k] * B[k][j]);
+        temp = (temp + A[i][k] * B[k][j]) % mod;
       }
       result[i][j] = temp;
     }
@@ -22,7 +24,7 @@ vector<vector<long long>> matMul(vector<vector<long long>> A, vector<vector<long
   return result;
 }
 
-void powOfA(vector<vector<long long>> &A, int n) {
+ vector<vector<long long>> powOfA(vector<vector<long long>> A, long long n) {
   vector<vector<long long>> powA;
   for(int i=0; i<100; i++) {
     vector<long long> temp(100,0);
@@ -37,8 +39,7 @@ void powOfA(vector<vector<long long>> &A, int n) {
     
     A = matMul(A,A);
   }
-
-  A = powA;
+  return powA;
 }
 
 long long laggedFib(long long n) {
@@ -59,12 +60,13 @@ long long laggedFib(long long n) {
     A.push_back(temp);
   }
 
-  powOfA(A,n-99);
+  A = powOfA(A,n-99);
 
   long long value = 0;
-  long long startVal = 99;
+  int startVal = 99;
   for(int i=0; i<100; i++) {
-    value += ((startVal--)*A[0][i]);
+    value = (value + A[0][i] * startVal) % mod;
+    startVal--;
   }
 
   return value;
